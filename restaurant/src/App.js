0b9b03d8a0e627a2d689cf49f3eb5796bb867f10
bedrantirak <bdrntrk@gmail.com>
2020-11-06 21:24:47 +0300

    Update App.js

--- restaurant/src/App.js
+++ restaurant/src/App.js
@@ -5,6 +5,7 @@ import Hero from './components/Hero/index';
 import Products from './components/Products';
 import { productData, productDataTwo } from './components/Products/data';
 import Feature from './components/Features';
+import Footer from './components/Footer';
 function App() {
   return (
     <Router>
@@ -13,6 +14,7 @@ function App() {
     <Products heading="Choose your favorite" data={productData} />
     <Feature/>
     <Products heading='Sweet Treats for You' data={productDataTwo} />
+    <Footer/>
     </Router>
   );
 }
