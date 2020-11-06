5240a53eab5b511d498451cb2fd9b5defb648b7c
bedrantirak <bdrntrk@gmail.com>
2020-11-06 16:07:17 +0300

    Update App.js

--- restaurant/src/App.js
+++ restaurant/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import {BrowserRouter as Router} from "react-router-dom"
 import { GlobalStyle } from './globalStyles';
 import Hero from './components/Hero/index';
-
+import Products from './components/Products';
 function App() {
   return (
     <Router>
       <GlobalStyle />
     <Hero />
+    <Products />
     </Router>
   );
 }
