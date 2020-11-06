15f61a05d2b0dc2950d49ac075c24a312dbdf192
bedrantirak <bdrntrk@gmail.com>
2020-11-06 21:04:41 +0300

    Update index.js

--- restaurant/src/components/Products/index.js
+++ restaurant/src/components/Products/index.js
@@ -1,10 +1,24 @@
 import React from 'react'
-import {ProductsContainer,ProductsHeading,ProductWrapper} from "./ProductsElements"
-function Products() {
+import {ProductButton, ProductCard, ProductDesc, ProductImg, ProductInfo, ProductPrice, ProductsContainer,ProductsHeading,ProductTitle,ProductWrapper} from "./ProductsElements"
+function Products({heading,data}) {
     return (
         <ProductsContainer>
-            <ProductsHeading>heading</ProductsHeading>
-            <ProductWrapper></ProductWrapper>
+            <ProductsHeading>{heading}</ProductsHeading>
+            <ProductWrapper>
+                {data.map((product,index)=>{
+                     return(
+                         <ProductCard key={index}>
+                             <ProductImg src={product.img} alt={product.alt}/>
+                             <ProductInfo>
+                             <ProductTitle>{product.name}</ProductTitle>  
+                             <ProductDesc>{product.desc}</ProductDesc>
+                             <ProductPrice>{product.price}</ProductPrice>
+                             <ProductButton>{product.button}</ProductButton>
+                             </ProductInfo>
+                         </ProductCard>
+                     )
+                })}
+            </ProductWrapper>
         </ProductsContainer>
     )
 }
