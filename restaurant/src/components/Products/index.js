b24abe91ba4688f77196fed71f991f9e61812dbd
bedrantirak <bdrntrk@gmail.com>
2020-11-06 16:10:15 +0300

    Update index.js

---
+++ restaurant/src/components/Products/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {ProductsContainer,ProductsHeading,ProductWrapper} from "./ProductsElements"
+function Products() {
+    return (
+        <ProductsContainer>
+            <ProductsHeading>heading</ProductsHeading>
+            <ProductWrapper></ProductWrapper>
+        </ProductsContainer>
+    )
+}
+
+export default Products
