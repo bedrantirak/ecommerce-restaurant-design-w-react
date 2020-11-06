824ceb42b4fda83e5d1ab3e07d3f4df4e71f6952
bedrantirak <bdrntrk@gmail.com>
2020-11-06 16:10:19 +0300

    Update ProductsElements.js

---
+++ restaurant/src/components/Products/ProductsElements.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const ProductsContainer = styled.div`
+  /* width: 100vw; */
+  min-height: 100vh;
+  padding: 5rem calc((100vw - 1300px) / 2);
+  background: #150f0f;
+  color: #fff;
+`;
+
+export const ProductWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: 0 auto;
+`;
+export const ProductsHeading = styled.h1`
+  font-size: clamp(2rem, 2.5vw, 3rem);
+  text-align: center;
+  margin-bottom: 5rem;
+`;
